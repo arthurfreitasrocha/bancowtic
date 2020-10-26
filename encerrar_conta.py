8f730c7439f50354e379366be2d1b2630462580c
Arthur Freitas <arthur.freitas2000@hotmail.com>
2020-10-26 17:52:47 -0300

    Update

--- encerrar_conta.py
+++ encerrar_conta.py
@@ -7,37 +7,14 @@ from sair import sairBanco
 
 import os
 
-def telaEncerrarConta():
+def telaEncerrarConta(login):
 
     os.system('cls')
 
-    print('\nBANCO WTIC - [TELA USUÁRIO -> CONFIGURAÇÕES DO USUÁRIO -> ENCERRAR CONTA]\n')
-
-    print('PARA CONFIRMAR A EXCLUSÃO DE SUA CONTA DIGITE SEU LOGIN E SUA SENHA\n\
-        OU DIGITE "sair" PARA SAIR.\n')
-
-    login = input('LOGIN: ')
-
-    if login.lower() == 'sair':
-        return False
-
-    senha = input('SENHA: ')
-
-    if senha.lower() == 'sair':
-        return False
-
-
-    return_validacao = validarLogin(login, senha)
+    return_validacao = validarLogin()
 
     if return_validacao == True:
         controleEncerrarConta(login)
-
-    else:
-
-        print('\nLOGIN E/OU SENHA INVÁLIDO(S)!\n')
-        os.system('pause')
-
-        return False
 
 
 def controleEncerrarConta(login):
